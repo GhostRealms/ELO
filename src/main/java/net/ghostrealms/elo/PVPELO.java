9c4bd84e4b958c71ff0970606fa0012369988773
River Marmorstein <river@rmarmorstein.com>
2016-02-28 15:19:10 -0500

    barebones database

--- src/main/java/net/ghostrealms/elo/PVPELO.java
+++ src/main/java/net/ghostrealms/elo/PVPELO.java
@@ -1,21 +1,32 @@
 package net.ghostrealms.elo;
 
+import net.ghostrealms.elo.lib.Database;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class PVPELO extends JavaPlugin {
   
+  private Database database;
+  
   @Override
   public void onEnable() {
-    
+    this.establishSQL();
   }
   
   @Override
   public void onDisable() {
-    
+    database.disconnect();
   }
   
-  private void setupDB(String host, String user, String pass, int port) {
-
+  private void establishSQL() {
+    String name = this.getConfig().getString("database.name");
+    String user = this.getConfig().getString("database.user");
+    String pass = this.getConfig().getString("database.pass");
+    String host = this.getConfig().getString("database.host");
+    int port = this.getConfig().getInt("database.port");
+    
+    database = new Database(name, host, user, pass, port);
+    database.connect();
   }
 
 }
