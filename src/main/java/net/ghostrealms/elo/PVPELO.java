2c3a9a5f9e60f2e00f09f1f590522743842c9a8f
River Marmorstein <river@rmarmorstein.com>
2016-02-28 14:56:30 -0500

    basic structuring, will be updated shortly

---
+++ src/main/java/net/ghostrealms/elo/PVPELO.java
@@ -0,0 +1,21 @@
+package net.ghostrealms.elo;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class PVPELO extends JavaPlugin {
+  
+  @Override
+  public void onEnable() {
+    
+  }
+  
+  @Override
+  public void onDisable() {
+    
+  }
+  
+  private void setupDB(String host, String user, String pass, int port) {
+
+  }
+
+}
