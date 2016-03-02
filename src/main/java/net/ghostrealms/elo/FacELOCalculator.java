a8973927362726b149bf01f08a62a70c103e658a
River Marmorstein <river@rmarmorstein.com>
2016-03-02 00:03:56 -0500

    add a start to an ELO calculator for factions related data

---
+++ src/main/java/net/ghostrealms/elo/FacELOCalculator.java
@@ -0,0 +1,13 @@
+package net.ghostrealms.elo;
+
+import java.util.UUID;
+
+public class FacELOCalculator {
+  
+  private UUID id;
+
+  public void getPower(UUID uuid) {
+    MPlayer player;
+  }
+
+}
